b8d08a092150ff9f61cbbe6a7f1afc06f70671ea
Goldman Park <death2501@gmail.com>
2021-10-25 22:22:21 +0900

    NLP(text preprocessing)

--- 04_basic_NLP.py
+++ 04_basic_NLP.py
@@ -1,9 +1,9 @@
 import csv
 import numpy as np
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Embedding, LSTM
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
+# extract words, sentences from csv file
 sentences = []
 labels = []
 stopwords = [ "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "could", "did", "do", "does", "doing", "down", "during", "each", "few", "for", "from", "further", "had", "has", "have", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "it", "it's", "its", "itself", "let's", "me", "more", "most", "my", "myself", "nor", "of", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "she", "she'd", "she'll", "she's", "should", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "we", "we'd", "we'll", "we're", "we've", "were", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "would", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves" ]
@@ -16,6 +16,19 @@ with open("./bbc-text.csv", 'r') as csvfile:
         sentence = row[1]
         for word in stopwords:
             token = " " + word + " "
-            sentence = sentence.replace(token, " ")
+            sentence = sentence.replace(token, " ") #remove
             sentence = sentence.replace("  ", " ")
         sentences.append(sentence)
+
+# sentence tokenize(create word dictionary)
+text_tokenizer = Tokenizer(oov_token="<OOV>", num_words=10000)
+text_tokenizer.fit_on_texts(sentences)
+print(text_tokenizer.word_index)
+print(text_tokenizer.word_counts)
+
+label_tokenizer = Tokenizer()
+label_tokenizer.fit_on_texts(labels)
+print(label_tokenizer.word_index)
+print(label_tokenizer.word_counts)
+
+# text padding
